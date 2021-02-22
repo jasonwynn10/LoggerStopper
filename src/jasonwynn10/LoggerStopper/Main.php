344d8f4581c79027896abbc4e7cb0bd439df7115
jasonwynn10 <jasonwynn10@me.com>
2021-02-22 13:35:42 -0500

    Fixed crash on server stop

--- src/jasonwynn10/LoggerStopper/Main.php
+++ src/jasonwynn10/LoggerStopper/Main.php
@@ -7,7 +7,16 @@ use pocketmine\plugin\PluginBase;
 class Main extends PluginBase {
 	public function onLoad() {
 		$this->getServer()->getLogger()->shutdown();
-		$this->getServer()->getLogger()->join();
+		//$this->getServer()->getLogger()->join();
 		unlink($this->getServer()->getDataPath()."server.log");
+	}
+	public function onDisable() {
+		touch($this->getServer()->getDataPath()."server.log");
+		$instance = $this->getServer()->getLogger();
+		$ref = new \ReflectionClass($instance);
+		$prop = $ref->getProperty('shutdown');
+		$prop->setAccessible(true);
+		$prop->setValue($instance, false);
+		$instance->notify();
 	}
 }
